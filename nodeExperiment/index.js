f309032eb324e454ae0a0a9d3a06fa1c02cc2fa9
Constantin Cezar Petrescu <petrescucc@gmail.com>
2016-12-09 01:53:51 +0000

    Setup the files for server with basic code to test

--- nodeExperiment/index.js
+++ nodeExperiment/index.js
@@ -1,2 +1,35 @@
-server.listen(3000);
+var express = require('express');
+var mysql = require('mysql');
+var http = require('http');
+var url  = require('url');
+var path = require('path');
+var fs   = require('fs');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var OSPoint = require('ospoint');
+var app = express();
+var server = http.createServer(app);
+// var jsonConcat = require('json-concat');
+// var Enc = require('cryptr'), cryptr = new Enc('qe432fdsrDSadsaj43221');
+// var json = require('json');
+// var m = require("./mail.js");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/planeadvisor'));
+app.use(cors());
+
+
+
+app.get('/',function(req, res)
+{
+        res.sendFile('../html5up-helios/index.html', { root : __dirname });
+});
+
+
+app.get('/',function(req, res)
+{
+        res.sendFile('www/index.html', { root : __dirname });
+});
+
+server.listen(8080);
 console.log('Express server started on port %s', server.address().port);
